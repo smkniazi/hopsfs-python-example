a9ad2d1b3c036fef0ffac151cae50d507ded8e6a
Salman Niazi <salman@logicalclocks.com>
2024-06-26 11:08:52 +0200

    Tests overriding dn address

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+#set -x
 source myenv/bin/activate
 
 
@@ -9,12 +10,12 @@ source myenv/bin/activate
 ##                 For testing on local machine 
 ##           
 ###############################################################################
-HADOOP_USER_NAME=salman   
-NAMENODE_IP=127.0.0.1
-NAMENODE_PORT=8020
-export     LIBHDFS_ROOT_CA_BUNDLE="/tmp/hopsfs-conf/certs/pems/salman_root_ca.pem"
-export LIBHDFS_CLIENT_CERTIFICATE="/tmp/hopsfs-conf/certs/pems/salman_certificate_bundle.pem"
-export         LIBHDFS_CLIENT_KEY="/tmp/hopsfs-conf/certs/pems/salman_private_key.pem"
+#HADOOP_USER_NAME=salman   
+#NAMENODE_IP=127.0.0.1
+#NAMENODE_PORT=8020
+##export     LIBHDFS_ROOT_CA_BUNDLE="/tmp/hopsfs-conf/certs/pems/salman_root_ca.pem"
+##export LIBHDFS_CLIENT_CERTIFICATE="/tmp/hopsfs-conf/certs/pems/salman_certificate_bundle.pem"
+##export         LIBHDFS_CLIENT_KEY="/tmp/hopsfs-conf/certs/pems/salman_private_key.pem"
 
             
 ###############################################################################
@@ -22,12 +23,12 @@ export         LIBHDFS_CLIENT_KEY="/tmp/hopsfs-conf/certs/pems/salman_private_ke
 ##                 For testing  in VM using hdfs user           
 ##           
 ###############################################################################
-#HADOOP_USER_NAME=hdfs
-#NAMENODE_IP=rpc.namenode.service.consul
-#NAMENODE_PORT=8020
-#export     LIBHDFS_ROOT_CA_BUNDLE="/srv/hops/super_crypto/hdfs/hops_root_ca.pem"
-#export LIBHDFS_CLIENT_CERTIFICATE="/srv/hops/super_crypto/hdfs/hdfs_certificate_bundle.pem"
-#export         LIBHDFS_CLIENT_KEY="/srv/hops/super_crypto/hdfs/hdfs_priv.pem"
+HADOOP_USER_NAME=hdfs
+NAMENODE_IP=rpc.namenode.service.consul
+NAMENODE_PORT=8020
+export     LIBHDFS_ROOT_CA_BUNDLE="/srv/hops/super_crypto/hdfs/hops_root_ca.pem"
+export LIBHDFS_CLIENT_CERTIFICATE="/srv/hops/super_crypto/hdfs/hdfs_certificate_bundle.pem"
+export         LIBHDFS_CLIENT_KEY="/srv/hops/super_crypto/hdfs/hdfs_priv.pem"
 
 
 ###############################################################################
@@ -52,9 +53,14 @@ export LIBHDFS_DEFAULT_FS="$NAMENODE_IP:$NAMENODE_PORT"
 export LIBHDFS_DEFAULT_USER="$HADOOP_USER_NAME"
 
 # for libhdfs logging
-export LIBHDFS_ENABLE_LOG="true"
+#export LIBHDFS_ENABLE_LOG="true"
 #export LIBHDFS_LOG_FILE="/tmp/libhdfs.log"
+
+export HOPSFS_CLOUD_DATANODE_HOSTNAME_OVERRIDE="localhost"
+#export HOPSFS_CLOUD_DATANODE_PORT_OVERRIDE")
+
 
 libhdfs_path=$(readlink -f libhdfs.so)
 export LD_PRELOAD=$libhdfs_path 
 python test.py 
+#python 
--- test.py
+++ test.py
@@ -1,5 +1,62 @@
 import pydoop.hdfs as hdfs
 
-t = hdfs.ls('/Projects');
+print("-------- Listing ----------");
+t = hdfs.ls('/');
 print(t)
+
+print("-------- Mkdir ----------");
+
+hdfs.mkdir("/tmp")
+
+print("-------- Delete a File ----------");
+
+hdfs_path = "/tmp/test_file.txt"
+
+hdfs.rm(hdfs_path);
+
+print("-------- Write a File ----------");
+
+# Function to generate 1MB of data
+def generate_data(size_in_mb):
+    return "a" * (size_in_mb * 1024 * 1024)  
+
+# Function to write data to HDFS
+def write_to_hdfs(hdfs_path, data):
+    try:
+        with hdfs.open(hdfs_path, 'w') as f:
+            s=f.write(data.encode('utf-8'))
+            f.close()
+        print(f"File written to {hdfs_path} successfully.")
+    except Exception as e:
+        print(f"An error occurred during write: {e}")
+
+data = generate_data(10)
+
+write_to_hdfs(hdfs_path, data)
+
+print("-------- Read a File ----------");
+
+# Function to read data from HDFS
+def read_from_hdfs(hdfs_path):
+    try:
+        with hdfs.open(hdfs_path, 'r') as f:
+            data = f.read().decode('utf-8')
+        print(f"File read from {hdfs_path} successfully.")
+        return data
+    except Exception as e:
+        print(f"An error occurred during read: {e}")
+        return None
+
+# Function to verify the data
+def verify_data(original_data, read_data):
+    if original_data == read_data:
+        print("Data verification successful: Data matches.")
+    else:
+        print("Data verification failed: Data does not match.")
+
+read_data = read_from_hdfs(hdfs_path)
+
+verify_data(data, read_data)
+
+
 
